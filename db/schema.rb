b6add8c893bdbcfcf32743589a0811518d64b0bc
2DN <74901304+2DN@users.noreply.github.com>
2021-01-22 23:14:56 +0900

    scoresテーブル　作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_134434) do
+ActiveRecord::Schema.define(version: 2021_01_22_141234) do
+
+  create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "japanese"
+    t.string "math"
+    t.string "science"
+    t.string "social"
+    t.string "english"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_scores_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_01_22_134434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "scores", "users"
 end

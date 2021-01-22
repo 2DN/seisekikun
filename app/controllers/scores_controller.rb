ddfb1caf8a729423590a4255bbe4dbe4840e04a4
2DN <74901304+2DN@users.noreply.github.com>
2021-01-22 23:26:30 +0900

    new, createアクション　編集

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,4 +2,24 @@ class ScoresController < ApplicationController
   before_action :authenticate_user!
   def index
   end
+
+  def new
+    @score = Score.new
+  end
+
+  def create
+    @score = Score.new(score_params)
+    if @score.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:japanese, :math, :science, :social, :english).merge(user_id: current_user.id)
+  end
+
 end
